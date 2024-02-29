c126f332f27dec54107e0ceee859bfdde74eac98
Jordan McKoy <jordan.bmp.mckoy@gmail.com>
2024-02-29 17:20:21 -0500

    Added dev logger

--- src/main/java/com/tropicoss/alfred/Alfred.java
+++ src/main/java/com/tropicoss/alfred/Alfred.java
@@ -2,6 +2,7 @@ package com.tropicoss.alfred;
 
 import net.fabricmc.api.DedicatedServerModInitializer;
 
+import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,8 @@ public class Alfred implements DedicatedServerModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("alfred");
 
+	public static FabricLoader fabricLoader = FabricLoader.getInstance();
+
 	@Override
 	public void onInitializeServer() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -18,5 +21,13 @@ public class Alfred implements DedicatedServerModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+	}
+
+	public static void devLogger(String loggerInput) {
+		// prevent usage if the Instance is not run in a development environment
+		if (!fabricLoader.isDevelopmentEnvironment()) return;
+
+		// customize that message however you want...
+		Alfred.LOGGER.info("DEV - [" + loggerInput + "]");
 	}
 }
