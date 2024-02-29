1f3cafb8f147433593ee784de6da55a898eac765
jordanmckoy <jordan.bmp.mckoy@gmail.com>
2024-02-28 21:56:38 -0500

    Updated discord bot

--- src/main/java/com/tropicoss/alfred/server/bot/DiscordBot.java
+++ src/main/java/com/tropicoss/alfred/server/bot/DiscordBot.java
@@ -4,6 +4,9 @@ import com.tropicoss.alfred.config.AppConfig;
 import com.tropicoss.alfred.config.ReadConfig;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 public class DiscordBot {
@@ -12,10 +15,22 @@ public class DiscordBot {
 
         JDABuilder builder = JDABuilder.createDefault(appConfig.getBotConfig().getToken());
 
-        // Disable parts of the cache
-        builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
+        builder.disableCache(CacheFlag.ACTIVITY,CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
+
+        builder.setMemberCachePolicy(MemberCachePolicy.ALL);
+
+        builder.setChunkingFilter(ChunkingFilter.ALL);
+
+        // Disable presence updates and typing events
+        builder.disableIntents(GatewayIntent.GUILD_PRESENCES, GatewayIntent.GUILD_MESSAGE_TYPING);
+
+        // Consider guilds with more than 50 members as "large".
+        // Large guilds will only provide online members in their setup and thus reduce bandwidth if chunking is disabled.
+        builder.setLargeThreshold(50);
+
         // Enable the bulk delete event
         builder.setBulkDeleteSplittingEnabled(false);
+
         // Set activity (like "playing Something")
         builder.setActivity(Activity.watching("TV"));
 
